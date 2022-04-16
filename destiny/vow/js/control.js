b73aca75adb45d9c5607535db58748b0044efe7e
Sherman Ying <fishermanying@gmail.com>
2022-04-16 13:02:46 -0400

    Implement Encounter 1 symbol filter toggle

--- destiny/vow/js/control.js
+++ destiny/vow/js/control.js
@@ -4,6 +4,13 @@ function closeFn(id) {
 function openFn(id) {
     return () => document.getElementById(id).classList.add('open')
 }
+function toggleEncounter(btn, id) {
+    return () => {
+        let val = (Number(document.getElementById(id).dataset.encounter) + 2) % 4;
+        document.getElementById(id).dataset.encounter = val;
+        btn.textContent = val;
+    }
+}
 
 window.addEventListener('load', function () {
     document.querySelectorAll('button[data-action="open"][data-target]').forEach(btn => {
@@ -12,4 +19,7 @@ window.addEventListener('load', function () {
     document.querySelectorAll('button[data-action="close"][data-target]').forEach(btn => {
         btn.onclick = closeFn(btn.dataset.target)
     });
+    document.querySelectorAll('button[data-action="encounter"]').forEach(btn => {
+        btn.onclick = toggleEncounter(btn, btn.dataset.target)
+    })
 })
